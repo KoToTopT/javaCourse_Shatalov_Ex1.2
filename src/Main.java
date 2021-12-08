14112770c8a949509933b48c7e83727df0cedd9b
out-shatalov-im <ishatalovat@gmail.com>
2021-12-08 17:41:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //Вариант 7
+        String university = "\"Универ не твоих собачьих дел\"";
+        int num_of_students = 20000;
+        double part_of_deduction = 10.5;
+        System.out.println("Название учебного учереждения: " + university + "; Количество студентов: " + num_of_students + "; Количество юродивых: " + part_of_deduction);
+    }
+}
